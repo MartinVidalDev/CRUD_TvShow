578836d74f2602d51e57bd18fa2fbb80023421d9
Malcolm Aridory <malcolm.aridory@etudiant.univ-reims.fr>
2024-06-10 15:40:51 +0200

    EpisodeCollection.php : findAll changé en findBySeasonId

--- src/Entity/Collection/EpisodeCollection.php
+++ src/Entity/Collection/EpisodeCollection.php
@@ -14,18 +14,21 @@ class EpisodeCollection
      * This static method executes a SQL query to select all columns from the `season` table,
      * ordered by the name of the episode. It returns the results as an array of `Episode` objects.
      *
+     * @param int $seasonId The season the episode is from
+     *
      * @return Episode[] An array of Episode objects.
      */
-    public static function findAll(): array
+    public static function findBySeasonid(int $seasonId): array
     {
         $request = MyPdo::getInstance()->prepare(
             <<<SQL
         SELECT id, seasonId, name, overview, episodeNumber
         FROM episode
+        WHERE seasonId = :seasonId
         ORDER BY episodeNumber;
         SQL
         );
-        $request->execute();
+        $request->execute(['seasonId' => $seasonId]);
         $request->setFetchMode(PDO::FETCH_CLASS, Episode::class);
         return $request->fetchAll();
     }
