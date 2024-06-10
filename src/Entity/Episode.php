fd90e7a3855bd242e876be033772eec22004f043
Malcolm Aridory <malcolm.aridory@etudiant.univ-reims.fr>
2024-06-10 14:53:39 +0200

    Ajout de la classe Episode avec ses attributs, accesseurs et modificateurs

---
+++ src/Entity/Episode.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+class Episode
+{
+    private int $id;
+    private int $seasonId;
+    private string $name;
+    private string $overview;
+    private int $episodeNumber;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getSeasonId(): int
+    {
+        return $this->seasonId;
+    }
+
+    public function setSeasonId(int $seasonId): void
+    {
+        $this->seasonId = $seasonId;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    public function setOverview(string $overview): void
+    {
+        $this->overview = $overview;
+    }
+
+    public function getEpisodeNumber(): int
+    {
+        return $this->episodeNumber;
+    }
+
+    public function setEpisodeNumber(int $episodeNumber): void
+    {
+        $this->episodeNumber = $episodeNumber;
+    }
+
+
+}
