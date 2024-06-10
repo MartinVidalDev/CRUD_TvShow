334e8f5b73b38613e2bae7813df6ccef3284a4d0
Malcolm Aridory <malcolm.aridory@etudiant.univ-reims.fr>
2024-06-10 09:44:28 +0200

    Ajout de la classe TVShow (attributs, accesseurs et modificateurs)

---
+++ src/Entity/TVShow.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Entity;
+
+class TVShow
+{
+    private int $id;
+    private string $name;
+    private string $originalName;
+    private string $homepage;
+    private string $overview;
+    private int $posterId;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getPosterId(): int
+    {
+        return $this->posterId;
+    }
+
+    public function setPosterId(int $posterId): void
+    {
+        $this->posterId = $posterId;
+    }
+
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    public function setOverview(string $overview): void
+    {
+        $this->overview = $overview;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getOriginalName(): string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(string $originalName): void
+    {
+        $this->originalName = $originalName;
+    }
+
+    public function getHomepage(): string
+    {
+        return $this->homepage;
+    }
+
+    public function setHomepage(string $homepage): void
+    {
+        $this->homepage = $homepage;
+    }
+
+
+}
