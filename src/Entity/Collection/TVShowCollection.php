233b50a5bf9694d6e3fb6931bf7908f3956f716a
Malcolm Aridory <malcolm.aridory@etudiant.univ-reims.fr>
2024-06-10 10:25:35 +0200

    TVShowCollection : ajout de la méthode findAll()

--- src/Entity/Collection/TVShowCollection.php
+++ src/Entity/Collection/TVShowCollection.php
@@ -4,7 +4,25 @@ namespace Entity\Collection;
 use Entity\TVShow;
 use PDO;
 use Database\MyPdo;
+
 class TVShowCollection
 {
+    /**
+     * Find all artists
+     *
+     * @return TVShow[]
+     */
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+        SELECT id, name, originalName, homepage, overview, posterId
+        FROM tvshow
+        ORDER BY name
+    SQL
+        );
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS, TVShow::class);
+    }
 
 }
