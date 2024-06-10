ea93794734f8d7c210fc845f911027ab741bae3a
Martin Vidal <martin.vidal@etudiant.univ-reims.fr>
2024-06-10 11:44:38 +0200

    Ajout de la méthode findAllSeason() dans SeasonCollection.php avec la documentation

--- src/Entity/Collection/SeasonCollection.php
+++ src/Entity/Collection/SeasonCollection.php
@@ -4,7 +4,31 @@ declare(strict_types=1);
 
 namespace Entity\Collection;
 
+use Database\MyPdo;
+use Entity\Season;
+use PDO;
+
 class SeasonCollection
 {
-
+    /**
+     * Retrieves all seasons from the database.
+     *
+     * This static method executes a SQL query to select all columns from the `season` table,
+     * ordered by the name of the season. It returns the results as an array of `Season` objects.
+     *
+     * @return Season[] An array of Season objects.
+     */
+    public static function findAllSeason(): array
+    {
+        $request = MyPdo::getInstance()->prepare(
+            <<<SQL
+        SELECT id, tvShow, name, seasonNumber, posterId
+        FROM season
+        ORDER BY name
+        SQL
+        );
+        $request->execute();
+        $request->setFetchMode(PDO::FETCH_CLASS, Season::class);
+        return $request->fetchAll();
+    }
 }
