1be65c91b2453cd6167c00b98325595390b73610
Martin Vidal <martin.vidal@etudiant.univ-reims.fr>
2024-06-11 09:30:52 +0200

    Restructuration de la structure html pour le style de tvshow.php

--- public/tvshow.php
+++ public/tvshow.php
@@ -29,12 +29,12 @@ foreach ($seasons as $season) {
     $posterId = (int)$webPage->escapeString("{$season->getPosterId()}");
     $name = $webPage->escapeString("{$season->getName()}");
     $seasonList .= <<<HTML
-        <li class='season'>
+        <div class='season'>
             <a href="season.php?seasonId={$season->getId()}">
                 <img class='season__poster' alt='{$name}' src='poster.php?posterId={$posterId}'>
                 <span class='season__name'>{$name}</span>
             </a>
-        </li>
+        </div>
     HTML;
 }
 
@@ -42,14 +42,14 @@ $webPage->appendContent(<<<HTML
 <div class='tvshow'>
         <img class='tvshow__poster' alt='{$show->getName()}' src='poster.php?posterId={$show->getPosterId()}' >
         <div class='tvshow__info'>
-            <span class='tvshow__name'>{$show->getName()}</span>
-            <span class='tvshow__originalName'>{$show->getOriginalName()}</span>
+            <span class='tvshow__name tvshow__title'>{$show->getName()}</span>
+            <span class='tvshow__originalName tvshow__title'>{$show->getOriginalName()}</span>
             <span class='tvshow__overview'>{$show->getOverview()}</span>
         </div>
 </div>
-    <ul class='list'>
+    <div class='list__season'>
     {$seasonList}
-    </ul>
+    </div>
 HTML);
 
 echo $webPage->toHTML();
