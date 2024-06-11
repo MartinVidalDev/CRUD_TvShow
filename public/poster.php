9f769c611fa3889a8e7870f1e99534e170fe39d0
vida0018 <martin.vidal@etudiant.univ-reims.fr>
2024-06-11 20:20:42 +0200

    Chemin résolu mais ne s'affiche pas encore dans index.php

--- public/poster.php
+++ public/poster.php
@@ -7,19 +7,21 @@ use Entity\Exception\EntityNotFoundException;
 use Entity\Exception\ParameterException;
 
 try {
+    $poster = file_get_contents('image/default-poster.png');
     if (!isset($_GET['posterId']) || !ctype_digit($_GET['posterId'])) {
         throw new ParameterException();
     }
 
     $poster = Poster::findById((int)$_GET['posterId']);
 
-    header('Content-Type: image/jpeg');
+    header('Content-Type: image/png');
     echo $poster->getJpeg();
 
 } catch (ParameterException) {
     http_response_code(400);
 } catch (EntityNotFoundException) {
-    file_get_contents('/image/default-poster.png');
+    header("Content-Type: image/png");
+    echo $poster;
 } catch (Exception) {
     http_response_code(500);
 }
