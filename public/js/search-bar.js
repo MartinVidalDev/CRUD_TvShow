d5a99ad100aee4162c8556f33d4b07f5a26746c2
Martin Vidal <martin.vidal@etudiant.univ-reims.fr>
2024-06-12 11:47:27 +0200

    Modification du script pour permettre de cliquer sur la loupe

--- public/js/search-bar.js
+++ public/js/search-bar.js
@@ -1,9 +1,11 @@
 function searchBar(tab) {
     const resultsBox = document.querySelector(".result__box");
     const inputBox = document.getElementById("input-box");
+    const searchIcon = document.querySelector('.search-field i');
 
-    inputBox.onkeyup = function() {
-        let result = [];
+    let result = [];
+
+    inputBox.onkeyup = function(event) {
         let input = inputBox.value;
         if (input.length > 0) {
             result = tab.filter((keyword) => {
@@ -16,14 +18,26 @@ function searchBar(tab) {
         if (!result.length) {
             resultsBox.innerHTML = '';
         }
+
+        // Si l'utilisateur appuie sur la touche "Entrée", redirige vers le premier résultat
+        if (event.key === 'Enter' && result.length > 0) {
+            window.location.href = 'tvshow.php?showId=' + result[0].id;
+        }
+    };
+
+    // Si l'utilisateur clique sur l'icône de recherche, redirige vers le premier résultat
+    searchIcon.onclick = function() {
+        if (result.length > 0) {
+            window.location.href = 'tvshow.php?showId=' + result[0].id;
+        }
     };
 
     function display(result) {
         const content = result.map((show) => {
-            return "<li onclick='selectInput(this)'><a href='tvshow.php?showId=" + show.id + "'>" + show.name + "</a></li>";
+            return "<li class='result__item' onclick='selectInput(this)'><a class='result__link' href='tvshow.php?showId=" + show.id + "'>" + show.name + "</a></li>";
         });
 
-        resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+        resultsBox.innerHTML = "<ul class='result__list'>" + content.join('') + "</ul>";
     }
 
     window.selectInput = function(selectedItem) {
